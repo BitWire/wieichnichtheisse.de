80669affb84390d57ab964d9deb415130784c3c4
Thomas Schmidt <thomas.schmidt@gastfreund.net>
2020-03-31 13:49:08 +0200

    Updates Readme, Adds Logo and adds Github link to footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,3 @@
 <footer>
-  <p>Made by: Thomas Schmidt | Contact information: <a href="https://deskhelper.de">deskhelper.de</a> | <a href="{{ url('gdpr') }}">GDPR</a></p>
+  <p>Made by: Thomas Schmidt | Contact information: <a href="https://deskhelper.de">deskhelper.de</a> |  <a href="https://github.com/BitWire/wieichnichtheisse.de">Github</a> | <a href="{{ url('gdpr') }}">GDPR</a></p>
 </footer> 
